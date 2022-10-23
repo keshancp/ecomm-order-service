a3d49feff894feb3466d9c45e1b1107ae2a5ebf9
keshancp <keshanc.perera@gmail.com>
2022-10-23 09:15:33 +0530

    order service initial commit

---
+++ src/main/java/com/ecomm/orderservice/EcommOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecomm.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommOrderServiceApplication.class, args);
+	}
+
+}
